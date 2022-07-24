db4c94941f66c441f1eab573601925e474ffa142
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2022-07-24 12:41:53 -0300

    First Commit

---
+++ bar3.py
@@ -0,0 +1,263 @@
+import colors
+from libqtile import widget
+from unicodes import left_arrow, right_arrow
+import os
+
+# COLORS FOR THE BAR
+colors = colors.gruvbox()
+
+def init_widgets_defaults():
+    return dict(font = "Hack NF",
+                fontsize = 12,
+                padding = 2,
+                background = colors[1])
+
+def init_widgets_list():
+    prompt = "{0}".format(os.environ["USER"])
+    widgets_list = [
+                widget.GroupBox(
+                        font = "Hack NF",
+                        fontsize = 25,
+                        margin_y = 3,
+                        margin_x = 0,
+                        padding_y = 6,
+                        padding_x = 5,
+                        borderwidth = 0,
+                        disable_drag = True,
+                        active = colors[6],
+                        inactive = colors[1],
+                        rounded = False,
+                        highlight_method = "text",
+                        this_current_screen_border = colors[9],
+                        foreground = colors[2],
+                        background = colors[7]
+                        ),
+                right_arrow(colors[6], colors[7]),
+                # widget.Sep(
+                #         linewidth = 1,
+                #         padding = 10,
+                #         foreground = colors[2],
+                #         background = colors[1]
+                #         ),
+                widget.CurrentLayoutIcon(
+                        custom_icon_paths = [os.path.expanduser("~/.config/qtile/icons/wmicons")],
+                        background = colors[6],
+                        padding = 0,
+                        scale = 0.7
+                        ),
+                widget.TextBox(
+                        font = "Hack NF Bold",
+                        fontsize = 16,
+                        text = "[",
+                        foreground = "#ffffff",
+                        background = colors[6],
+                        padding = 0,
+                        ),
+                widget.CurrentLayout(
+                        font = "Hack NF Bold",
+                        fontsize = 14,
+                        foreground = "#ffffff",
+                        background = colors[6]
+                        ),
+                widget.TextBox(
+                        font = "Hack NF Bold",
+                        fontsize = 16,
+                        text = "]",
+                        foreground = "#ffffff",
+                        background = colors[6],
+                        padding = 0,
+                        ),
+                right_arrow(colors[1], colors[6]),
+                # widget.Sep(
+                #         linewidth = 1,
+                #         padding = 10,
+                #         foreground = colors[2],
+                #         background = colors[1]
+                #         ),
+                widget.TextBox(
+                        font = "Hack NF Bold",
+                        fontsize = 14,
+                        text = " ",
+                        background = colors[1],
+                        padding = 0,
+                        ),
+                widget.WindowName(
+                        font = "Hack NF Italic",
+                        fontsize = 14,
+                        foreground = colors[5],
+                        background = colors[1],
+                        ),
+                # widget.TextBox(
+                #         font = "Hack NF",
+                #         text = "",
+                #         foreground = colors[2],
+                #         background = colors[1],
+                #         padding = 0,
+                #         fontsize = 16
+                #         ),
+                # widget.ThermalSensor(
+                #         font = "Hack NF Bold",
+                #         foreground = colors[5],
+                #         foreground_alert = colors[6],
+                #         background = colors[1],
+                #         metric = True,
+                #         padding = 3,
+                #         threshold = 80
+                #         ),
+                # battery option 2  from Qtile
+                # widget.Sep(
+                #         linewidth = 1,
+                #         padding = 10,
+                #         foreground = colors[2],
+                #         background = colors[1]
+                #         ),
+                # widget.Battery(
+                #         font = "Hack NF",
+                #         update_interval = 10,
+                #         fontsize = 12,
+                #         foreground = colors[5],
+                #         background = colors[1],
+	            #         ),
+                # widget.Sep(
+                #         linewidth = 1,
+                #         padding = 10,
+                #         foreground = colors[2],
+                #         background = colors[1]
+                #         ),
+                left_arrow(colors[1], colors[6]),
+                widget.DF(
+                        font = "Hack NF Bold",
+                        fontsize = 12,
+                        foreground = "#ffffff",
+                        background = colors[6],
+                        format = '{uf} {m}b',
+                        visible_on_warn = False
+                        ),
+                widget.TextBox(
+                        font = "FontAwesome",
+                        text = "  ",
+                        foreground = "#ffffff",
+                        background = colors[6],
+                        padding = 0,
+                        fontsize = 16
+                        ),
+                left_arrow(colors[6], colors [7]),
+                widget.Memory(
+                        font = "Hack NF Bold",
+                        # format = '{MemUsed: .2f} Gb /{MemTotal: .2f} Gb',
+                        format = '{MemUsed: .2f} Gb',
+                        measure_mem = 'G',
+                        update_interval = 1,
+                        fontsize = 12,
+                        foreground = colors[1],
+                        background = colors[7],
+                        ),
+                widget.TextBox(
+                        font = "FontAwesome",
+                        text = "  ",
+                        foreground = colors[1],
+                        background = colors[7],
+                        padding = 0,
+                        fontsize = 16
+                        ),
+                # widget.Sep(
+                #         linewidth = 1,
+                #         padding = 10,
+                #         foreground = colors[2],
+                #         background = colors[1]
+                #         ),
+                left_arrow(colors[7], colors[6]),
+                widget.KeyboardLayout(
+                        font = "Hack NF Bold",
+                        foreground = "#ffffff",
+                        background = colors[6],
+                        padding = 0,
+                        fontsize = 12,
+                        max_chars = 2,
+                        configured_keyboards = ['latam', 'us'],
+                        display_map = {'latam': 'LA', 'us': 'US'}
+                       ),
+                widget.TextBox(
+                        font = "FontAwesome",
+                        text = "  ",
+                        foreground = "#ffffff",
+                        background = colors[6],
+                        padding = 0,
+                        fontsize = 16
+                        ),
+                # widget.Sep(
+                #         font = "Hack NF Bold",
+                #         linewidth = 1,
+                #         padding = 10,
+                #         foreground = colors[2],
+                #         background = colors[1]
+                #         ),
+                left_arrow(colors[6], colors[7]),
+                widget.Clock(
+                        font = "Hack NF Bold",
+                        foreground = colors[1],
+                        background = colors[7],
+                        fontsize = 14,
+                        # format = "%a %d/%m/%y %H:%M",
+                        format = "%a %d/%m %H:%M",
+                        ),
+                widget.TextBox(
+                        font = "FontAwesome",
+                        text = "  ",
+                        foreground = colors[1],
+                        background = colors[7],
+                        padding = 0,
+                        fontsize = 16
+                        ),
+                # widget.Sep(
+                #         linewidth = 1,
+                #         padding = 10,
+                #         foreground = colors[2],
+                #         background = colors[1]
+                #         ),
+                left_arrow(colors[7], colors[6]),
+                widget.Systray(
+                        background = colors[6],
+                        icon_size = 20,
+                        padding = 4
+                        ),
+                # widget.Sep(
+                #         linewidth = 1,
+                #         padding = 10,
+                #         foreground = colors[2],
+                #         background = colors[1]
+                #         ),
+                left_arrow(colors[7], colors[6]),
+                left_arrow(colors[6], colors[1]),
+                widget.TextBox(
+                        font = "Hack NF Bold",
+                        text = prompt,
+                        foreground = colors[6],
+                        background = colors[1],
+                        padding = 0,
+                        fontsize = 16
+                        ),
+                widget.TextBox(
+                        font = "FontAwesome",
+                        text = "  ",
+                        foreground = colors[6],
+                        background = colors[1],
+                        padding = 0,
+                        fontsize = 16
+                        ),
+    ]
+    return widgets_list
+
+widget_defaults = init_widgets_defaults()
+
+widgets_list = init_widgets_list()
+
+def init_widgets_screen1():
+    widgets_screen1 = init_widgets_list()
+    # del widgets_screen1[23:27]
+    return widgets_screen1
+
+def init_widgets_screen2():
+    widgets_screen2 = init_widgets_list()
+    # del widgets_screen2[21:27]
+    return widgets_screen2
---
+++ colors.py
@@ -0,0 +1,65 @@
+# COLORS FOR THE BAR
+# Theme name : ArcoLinux Crimson
+def crimson():
+    return [["#4a4a46", "#4a4a46"], # color 0
+            ["#4a4a46", "#4a4a46"], # color 1
+            ["#c0c5ce", "#c0c5ce"], # color 2
+            ["#d33682", "#d33682"], # color 3
+            ["#cf3e3e", "#cf3e3e"], # color 4
+            ["#fdf6e3", "#fdf6e3"], # color 5
+            ["#d42121", "#d42121"], # color 6
+            ["#62FF00", "#62FF00"], # color 7
+            ["#cf3e3e", "#cf3e3e"], # color 8
+            ["#eb9b9b", "#eb9b9b"]] # color 9
+
+# Theme name : ArcoLinux Default
+def default():
+    return [["#2F343F", "#2F343F"], # color 0
+            ["#2F343F", "#2F343F"], # color 1
+            ["#c0c5ce", "#c0c5ce"], # color 2
+            ["#fba922", "#fba922"], # color 3
+            ["#3384d0", "#3384d0"], # color 4
+            ["#f3f4f5", "#f3f4f5"], # color 5
+            ["#cd1f3f", "#cd1f3f"], # color 6
+            ["#62FF00", "#62FF00"], # color 7
+            ["#6790eb", "#6790eb"], # color 8
+            ["#a9a9a9", "#a9a9a9"]] # color 9
+
+# Theme name : ArcoLinux Gruvbox
+def gruvbox():
+    return [["#282828", "#282828"], # color 0
+            ["#3c3836", "#3c3836"], # color 1
+            ["#98971a", "#98971a"], # color 2
+            ["#d79921", "#d79921"], # color 3
+            ["#458588", "#458588"], # color 4
+            ["#ebdbb2", "#ebdbb2"], # color 5
+            ["#83a598", "#83a598"], # color 6
+            ["#fbf1c7", "#fbf1c7"], # color 7
+            ["#1d2021", "#1d2021"], # color 8
+            ["#cc241d", "#cc241d"]] # color 9
+
+# Theme name : ArcoLinux Nord
+def nord():
+    return [["#3B4252", "#3B4252"], # color 0
+            ["#2e3440", "#2e3440"], # color 1
+            ["#A3BE8C", "#A3BE8C"], # color 2
+            ["#EBCB8B", "#EBCB8B"], # color 3
+            ["#81A1C1", "#81A1C1"], # color 4
+            ["#D8DEE9", "#D8DEE9"], # color 5
+            ["#88C0D0", "#88C0D0"], # color 6
+            ["#E5E9F0", "#E5E9F0"], # color 7
+            ["#4C566A", "#4C566A"], # color 8
+            ["#BF616A", "#BF616A"]] # color 9
+
+# Theme name : ArcoLinux Zion
+def zion():
+    return [["#4a4a46", "#4a4a46"], # color 0
+            ["#4a4a46", "#4a4a46"], # color 1
+            ["#e3bbf1", "#e3bbf1"], # color 2
+            ["#d33682", "#d33682"], # color 3
+            ["#3384d0", "#3384d0"], # color 4
+            ["#fdf6e3", "#fdf6e3"], # color 5
+            ["#d42121", "#d42121"], # color 6
+            ["#62FF00", "#62FF00"], # color 7
+            ["#9742b5", "#9742b5"], # color 8
+            ["#002b36", "#002b36"]] # color 9
