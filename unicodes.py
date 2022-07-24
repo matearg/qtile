3d753f76d1cfdd3bff6d6c17ffdbad951a36c6e8
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2022-07-24 15:11:04 -0300

    Removed unused code

--- unicodes.py
+++ unicodes.py
@@ -8,7 +8,6 @@ def left_half_circle(bg_color, fg_color):
         foreground=fg_color,
         padding=0)
 
-
 def right_half_circle(bg_color, fg_color):
     return TextBox(
         text='\uE0B4',
@@ -17,16 +16,6 @@ def right_half_circle(bg_color, fg_color):
         foreground=fg_color,
         padding=0)
 
-
-def lower_left_triangle(bg_color, fg_color):
-    return TextBox(
-        text='\u25e2',
-        padding=0,
-        fontsize=50,
-        background=bg_color,
-        foreground=fg_color)
-
-
 def left_arrow(bg_color, fg_color):
     return TextBox(
         text='\uE0B2',
@@ -34,7 +23,6 @@ def left_arrow(bg_color, fg_color):
         fontsize=24,
         background=bg_color,
         foreground=fg_color)
-
 
 def right_arrow(bg_color, fg_color):
     return TextBox(
