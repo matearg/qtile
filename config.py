173c5035c6dbf507edb43b1435f27a615d8555d0
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2022-07-25 18:53:34 -0300

    Removing redundant code

--- config.py
+++ config.py
@@ -151,14 +151,12 @@ keys.extend([
 groups = []
 
 # FOR QWERTY KEYBOARDS
-# group_names = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",]
 group_names = ["1", "2", "3", "4", "5",]
 
-# group_labels = [" 1", " 2", " 3", " 4", " 5",]
+# group_labels = ["", "", "", "", "",]
 group_labels = ["", "", "", "", "",]
 
 group_layouts = ["monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall",]
-# group_layouts = ["monadtall", "matrix", "monadtall", "bsp", "monadtall", "matrix", "monadtall", "bsp", "monadtall", "monadtall",]
 
 for i in range(len(group_names)):
     groups.append(
@@ -175,14 +173,9 @@ for i in groups:
         Key([mod], i.name, lazy.group[i.name].toscreen()),
         Key([mod], "Tab", lazy.screen.next_group()),
         Key([mod, "shift" ], "Tab", lazy.screen.prev_group()),
-        # Key(["mod1"], "Tab", lazy.screen.next_group()),
-        # Key(["mod1", "shift"], "Tab", lazy.screen.prev_group()),
 
 # MOVE WINDOW TO SELECTED WORKSPACE 1-10 AND STAY ON WORKSPACE
         Key([mod, "shift"], i.name, lazy.window.togroup(i.name)),
-
-# MOVE WINDOW TO SELECTED WORKSPACE 1-10 AND FOLLOW MOVED WINDOW TO WORKSPACE
-        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name) , lazy.group[i.name].toscreen()),
     ])
 
 userBar = bar1
@@ -199,8 +192,6 @@ layout_theme = init_layout_theme()
 layouts = [
     layout.MonadTall(**layout_theme),
     layout.MonadWide(**layout_theme),
-    # layout.Floating(**layout_theme),
-    # layout.Max(**layout_theme)
 ]
 
 widgets_screen1 = userBar.init_widgets_screen1()
