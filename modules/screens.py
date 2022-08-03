f8b9cd302f5e4124cc30fe19b648fb5efac75456
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2022-08-03 20:13:50 -0300

    Added rounde conrners

--- modules/screens.py
+++ modules/screens.py
@@ -1,6 +1,6 @@
 from libqtile import bar
 from libqtile.config import Screen
-from .widgetsvariant import widgets_screen1, widgets_screen2
+from .widgets import widgets_screen1, widgets_screen2
 
 def init_screens():
     return [
