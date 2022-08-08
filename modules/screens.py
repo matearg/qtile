188db702225f4a61bda0af513b2580f2d2f322cd
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2022-08-08 18:03:26 -0300

    Changed picom

--- modules/screens.py
+++ modules/screens.py
@@ -1,11 +1,11 @@
 from libqtile import bar
 from libqtile.config import Screen
-from .widgetsvariant3 import widgets_screen1, widgets_screen2
+from .widgets import widgets_screen1, widgets_screen2
 
 def init_screens():
     return [
-            # Screen(top=bar.Bar(widgets = widgets_screen1, opacity = 1, size = 30, margin = [8, 8, 0, 8])),
-            Screen(top=bar.Bar(widgets = widgets_screen1, opacity = 1, size = 28, margin = 0)),
+            Screen(top=bar.Bar(widgets = widgets_screen1, opacity = 1, size = 30, margin = [8, 8, 0, 8])),
+            # Screen(top=bar.Bar(widgets = widgets_screen1, opacity = 1, size = 30, margin = 0)),
             # Screen(top=bar.Bar(widgets = widgets_screen2, opacity = 1, size = 30, margin = [8, 8, 0, 8]))
     ]
 
--- modules/widgets.py
+++ modules/widgets.py
@@ -363,8 +363,7 @@ def init_widgets_list():
                         font=mono_font,
                         fontsize=20,
                         padding=16,
-                        # mouse_callbacks={"Button1": lazy.spawn("archlinux-logout")},
-                        mouse_callbacks={"Button1": lazy.spawn("rofi -show power-menu -modi power-menu:rofi-power-menu")},
+                        mouse_callbacks={"Button1": lazy.spawn("rofi -show power-menu -modi power-menu:/home/mateo/github/dotfiles/linux/rofi/rofi-power-menu")},
                         ),
     ]
     return widgets_list
