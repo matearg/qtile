f2cf283378222cfed663fd0fe319ffe16b50c973
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2022-08-10 15:13:30 -0300

    Updated screenshots

--- modules/screens.py
+++ modules/screens.py
@@ -5,7 +5,7 @@ from .widgetsvariant4 import widgets_screen1
 def init_screens():
     return [
             # Screen(top=bar.Bar(widgets = widgets_screen1, opacity = 1, size = 30, margin = [10, 10, 0, 10])),
-            Screen(top=bar.Bar(widgets = widgets_screen1, opacity = 1, size = 28, margin = 0)),
+            Screen(top=bar.Bar(widgets = widgets_screen1, opacity = 1, size = 30, margin = 0)),
     ]
 
 screens = init_screens()
