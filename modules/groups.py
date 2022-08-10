306697f65123f077589e20ef09a1c7a0982544ec
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2022-08-10 01:03:26 -0300

    Added a lil of rome

--- modules/groups.py
+++ modules/groups.py
@@ -2,11 +2,14 @@ from libqtile.config import Group
 
 groups = []
 
-group_names = ["1", "2", "3", "4", "5",]
+
+# group_names = ["1", "2", "3", "4", "5",]
+group_names = ["1", "2", "3", "4", "5", "6", "7", "8", "9",]
 
 # group_labels = ["", "", "", "", "",]
-group_labels = ["1", "2", "3", "4", "5",]
+# group_labels = ["1", "2", "3", "4", "5",]
 # group_labels = ["", "", "", "", "",]
+group_labels = ["I", "II", "II", "IV", "V", "VI", "VII", "VIII", "IX",]
 
 group_layouts = ["monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall",]
 
